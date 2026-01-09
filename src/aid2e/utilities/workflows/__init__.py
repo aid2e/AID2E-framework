e8ce901d239e9e31286009926df4af298d8ae789
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-01-09 18:46:04 -0500

    Begin implementing layer base class

---
+++ src/aid2e/utilities/workflows/__init__.py
@@ -0,0 +1,11 @@
+"""Workflow utilities for AID2E Framework"""
+
+from adi2e import __MAIN_VERSION__
+
+__version__ = __MAIN__VERSION__
+
+from .experimental_stack import StackLayer
+
+__all__ = [
+	"StackLayer",
+]
---
+++ src/aid2e/utilities/workflows/experimental_stack.py
@@ -0,0 +1,126 @@
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class StackLayer(ABC):
+    """Represents a layer of a software stack
+
+    Abstract base class that represents a layer of an experimental software
+    stack. A layer is defined by a unique name, a specific command to be run,
+    and a rule which dictates how arguments are combined.
+
+    Properties:
+        name: Unique name for the layer.
+        command: Command to be run (e.g. npsim)
+        rule: Recipe for combining the command and provided arguments
+              using keywords, e.g. 'command args outputs inputs'
+
+    Example:
+        >>> layer = ExperimentLayer(
+        ...     name="sim",
+        ...     command="npsim",
+        ...     rule="exec args outputs"
+        ... )
+        ... run_layer = layer.make_command(
+        ...     inputs,
+        ...     outputs,
+        ...     arguments
+        ... )
+    """
+    @property
+    @abstractmethod
+    def name(self):
+        """Name of this layer (e.g. Sim)
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def command(self):
+        """Command to be run (e.g. npsim)
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def rule(self):
+        """Recipe for combining command and arguments
+        """
+        pass
+
+    @property
+    @
+    command: str
+    rule: str
+
+    @abstractmethod
+    def _make_input_arg(self, inputs: List[str]) -> str:
+        """Make input argument
+
+        Converts provided list of input filee into string of properly formatted
+        inputs for command.
+
+        Args:
+            inputs: List of input files
+        Returns:
+            String of formatted input files
+        """
+        pass
+
+    @abstractmethod
+    def _make_output_arg(self, outputs: List[str]) -> str:
+        """Make output argument
+
+        Converts provided list of output files into string of properly formatted
+        outputs for command.
+
+        Args:
+            outputs: List of output files
+        Returns:
+            String of formatted outputs
+        """
+        pass
+
+    def _make_other_arg(self arguments: List[str]) -> str:
+        """Make other arguments
+
+        By default, joins provided list of arguments into a space-separated
+        string. Can be overwritten for behavior unique to specific layers.
+
+        Args:
+            arguments: List of arguments to join
+        Returns:
+            String of formatted arguments
+        """
+        return ' '.join(arguments)
+
+    def make_command(self, inputs: str, outputs: str, arguments:str = None) -> str:
+        """Make command
+
+        Returns command to run with all inputs outputs, and arguments formatted
+        according to layer rule.
+
+        Args:
+            inputs: List of input files
+            outputs: List of output files
+            arguments: Optional ist of additional arguments
+        """
+        # format and sub in inputs/outputs
+        in_arg = self._make_input_arg(inputs)
+        out_arg = self._make_output_arg(outputs)
+        command = self.command.replace('inputs', in_arg)
+        command = command.replace('outputs', out_arg)
+
+        # if needed, sub in any other arguments
+        if arguments != None:
+            other_arg = self._make_other_arg(arguments)
+            command = command.replace('arguments', other_arg)
+        else:
+            command = command.replace('arguments', '')
+
+        # return formatted command without any
+        # stray double spaces
+        return command.replace('  ', ' ')

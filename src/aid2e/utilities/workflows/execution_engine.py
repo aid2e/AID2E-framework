ce347ddeaa93c098879a018e09d031632a74a26d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-05-13 18:43:47 -0400

    Add alias for geometry directory

--- src/aid2e/utilities/workflows/execution_engine.py
+++ src/aid2e/utilities/workflows/execution_engine.py
@@ -720,6 +720,7 @@ class StackExecutionEngine(BaseExecutionEngine):
         result = text
         result = result.replace("{{context.job_id}}", str(context.job_id))
         result = result.replace("{{context.execution_dir}}", str(context.execution_dir))
+        result = result.replace("{{context.geometry_dir}}", str(context.workflow_context.parameters["prepared_geometry_dir"]))
         for key, value in context.design_point.items():
             result = result.replace(f"{{design_point.{key}}}", str(value))
         return result

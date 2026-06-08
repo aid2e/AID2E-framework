ce0d90fbe1919d527fdbbefc58a9c4676fb031bf
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-06-08 17:39:12 -0400

    Test that we can make an executor from an EpicWorkflow

--- tests/test_utilities/test_epic_utils/test_epic_stack_config.py
+++ tests/test_utilities/test_epic_utils/test_epic_stack_config.py
@@ -10,6 +10,7 @@ from aid2e.utilities.epic_utils.epic_stack_config import (
     EpicWorkflowDefinition,
     EpicWorkflowsConfiguration,
 )
+from aid2e.utilities.workflows.dag_executor import DAGExecutor
 
 def _sample_epic_workflow_payload() -> dict:
     """Build ePIC workflow configuration."""
@@ -161,3 +162,19 @@ def test_epic_workflows_validation(tmp_path):
     assert "epic" == config.workflows[0].branches[0].stages[1].jobs[0].payload["stack_type"]
     assert "rec" == config.workflows[0].branches[0].stages[2].jobs[0].layers[0].name
     assert "-s 22" == config.workflows[0].branches[0].stages[2].jobs[0].layers[1].arguments[1]
+
+def test_epic_executor_from_config(tmp_path):
+    """Create DAGExecutor from epic workflow."""
+    config_path = tmp_path / "epic_workflow.config"
+    config_path.write_text(yaml.safe_dump(_sample_epic_workflow_payload()))
+
+    payload = None
+    with open(config_path, 'r') as data:
+        payload = yaml.safe_load(data)
+    assert payload is not None
+
+    config = payload["workflows"][0]
+    workflow = EpicWorkflowDefinition(**config)
+    executor = DAGExecutor(workflow, base_output_dir=tmp_path)
+    assert executor is not None
+    assert "photon_phi_resolution" == executor.workflow.branches[0].name

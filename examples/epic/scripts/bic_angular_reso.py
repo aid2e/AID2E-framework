6847332e5711ed34e8b8372416e5633a5ab2f889
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-08-27 15:49:09 -0400

    Conform extractor to API

--- examples/epic/scripts/bic_angular_reso.py
+++ examples/epic/scripts/bic_angular_reso.py
@@ -305,11 +305,29 @@ def CalculateHitAngReso(opts: Options = DEFAULT_OPTS) -> Dict[str, float]:
 # =============================================================================
 # Extract objective from an output file
 # =============================================================================
-def ExtractObjective(*, key, file, **_):
+def ExtractObjective(*, extra_args, **_):
     """Extract objective
 
-       MORE HERE
+       Extracts a metric based from the JSON file
+       written by CalculateHitAngReso. The key
+       of the value to extract and the path to
+       the JSON file must be provided through
+       the `extra_args` block.
+
+       Args:
+           extra_args: dictionary of extra arugments
+                       must contain `key` and `file`
+       Returns:
+           extracted metric formatted as a dictionary
     """
+    if 'key' not in extra_args:
+        raise KeyError(f"'key' not found in extra_args! extra_args = {extra_args}")
+
+    if 'file' not in extra_args:
+        raise KeyError(f"'file' not found in extra_args! extra_args = {extra_args}")
+    key  = extra_args['key']
+    file = extra_args['file']
+
     metrics = {}
     with open(file, 'r') as f:
         data         = json.load(f)
